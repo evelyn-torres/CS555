3e0c7bf8edfbd1bcb8d8d300b492675e9616b842
Evelyn Torres <etorres.et905@gmail.com>
2024-10-17 16:51:42 -0400

    add frontend react intialization

---
+++ AI/well_being_chatbot/ai_response/test_well_being_convo.py
@@ -0,0 +1,32 @@
+import unittest
+from well_being_chatbot.well_being import AIConvo
+
+class TestAIConvo(unittest.TestCase):
+    def setUp(self):
+        self.ai = AIConvo()
+
+    def test_ask_question(self):
+        question = self.ai.ask_question1()
+        self.assertEqual(question, "How are you feeling today?")
+    def test_log_response(self):
+        response = "I am feeling fine"
+        self.ai.log_response(response)
+        self.assertIn(response, self.ai.get_responses())
+   
+    def test_other_questions(self):
+        valid_questions =[
+            "Did you sleep well last night?",
+            "Have you eaten anything good today?",
+            "Did you manage to go for a walk today?",
+            "How’s your energy level today?"
+        ]
+        question = self.ai.ask_other_questions()
+        self.assertIn(question,valid_questions)
+
+    def test_empty_response(self):
+        response = ""
+        self.ai.log_response(response)
+        self.assertIn(response, self.ai.get_responses())
+
+if __name__ == "__main__":
+    unittest.main()
