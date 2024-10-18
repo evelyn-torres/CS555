9b2439db9e0331dfcf38527fb015429969430ae3
Nicholas Mohamed <144374109+nicholasm387@users.noreply.github.com>
2024-10-17 21:04:50 -0400

    login page
    Simple login in page

---
+++ FrontEnd/frontEnd/src/pages/Login.jsx
@@ -0,0 +1,84 @@
+// src/pages/Login.jsx
+
+import React, { useState } from 'react';
+import { Button, TextField, Box, Typography, Container, Avatar } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+const Login = () => {
+  // State to handle the email and password 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // check check 
+    console.log({ email, password });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {/* Lock icon */}
+        <Avatar sx={{ m: 1, bgcolor: 'purple' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+       
+
+        {/* Title */}
+        <Typography component="h1" variant="h5">
+          Log In
+        </Typography>
+
+        {/* the form */}
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          {/* Email input */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          {/* Password input */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* Submit button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
