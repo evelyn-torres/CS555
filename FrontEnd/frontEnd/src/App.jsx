f9c69b7fe06e2dabc6435a39eb725c9eb376a034
Nicholas Mohamed <nmohame2@stevens.edu>
2024-10-27 15:10:50 -0400

    backend set up for the sucure login system

--- FrontEnd/frontEnd/src/App.jsx
+++ FrontEnd/frontEnd/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './styles/App.css'
 import Login from './pages/Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
 
 
   return (
+    <Router>
       <div>
-        <Login />
+        <li><Link to="/">Login</Link></li>
       </div>
+    </Router>
         
   )
 }
