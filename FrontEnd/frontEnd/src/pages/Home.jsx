1da2770c83f5d375322ea51586a6521ff5a2d437
Elizabeth Bruski <ebruski@stevens.edu>
2024-11-01 18:30:30 -0400

    committing the frontend changes so they can be merged#
    :wq

--- FrontEnd/frontEnd/src/pages/Home.jsx
+++ FrontEnd/frontEnd/src/pages/Home.jsx
@@ -1,14 +1,28 @@
 // import react from "@vitejs/plugin-react-swc";
 // src/pages/Home.jsx
 
-import React from 'react';
-
+import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Homepage!</h1>
-      <p>This is the home page after logging in.</p>
+    <div className="home-container">
+      <div class="header">
+        <h1>Vitalink Voice Assist</h1>
+        <h2>Click a button if you need assistance.</h2>
+      </div>
+      <button class="question-button button big-button">
+        Ask a Question
+        {/* <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Login
+        </NavLink> */}
+      </button>
+      <button class="request-button button big-button">
+        Request a Healthy Activity
+      </button>
     </div>
   );
 };
